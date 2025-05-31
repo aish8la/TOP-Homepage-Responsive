78e253be274781a680afaf719719824c0f8d24ae
aish8la <64914670+aish8la@users.noreply.github.com>
2025-05-31 19:26:41 +0500

    build: add webpack loaders and plugins

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,5 @@
+const { rules } = require('eslint-config-prettier');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,4 +9,26 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
