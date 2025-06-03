569dcb07c41623a24bb9b5bba9a48e75d185bf75
aish8la <64914670+aish8la@users.noreply.github.com>
2025-06-04 01:31:00 +0500

    style: added formatting to code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 import "./styles.css";
 
-
-
 requestAnimationFrame(() => {
-    document.body.classList.add("active");
-    document.body.classList.remove("fade-in");
+  document.body.classList.add("active");
+  document.body.classList.remove("fade-in");
 });
 
 function entryClassCb(entries, observer) {
-    entries.forEach((entry, index) => {
-        if(entry.isIntersecting) {
-            entry.target.style.transitionDelay = `${index * 0.2}s`;
-            entry.target.classList.add("active");
-            observer.unobserve(entry.target);
-        }
-    });
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      entry.target.style.transitionDelay = `${index * 0.2}s`;
+      entry.target.classList.add("active");
+      observer.unobserve(entry.target);
+    }
+  });
 }
 
 const observerOptions = {
-    threshold: 0.5,
-}
+  threshold: 0.5,
+};
 
 const observer = new IntersectionObserver(entryClassCb, observerOptions);
 
-document.querySelectorAll('.card-fade').forEach(ele => {
-    observer.observe(ele);
+document.querySelectorAll(".card-fade").forEach((ele) => {
+  observer.observe(ele);
 });
