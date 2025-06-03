2a08bfa18e5cf563363316475e1fef6d6133757e
aish8la <64914670+aish8la@users.noreply.github.com>
2025-06-04 01:18:37 +0500

    feat: add transition to card elements
    Transitions added using Intersection Observer API

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ requestAnimationFrame(() => {
     document.body.classList.add("active");
     document.body.classList.remove("fade-in");
 });
+
+function entryClassCb(entries, observer) {
+    entries.forEach((entry, index) => {
+        if(entry.isIntersecting) {
+            entry.target.style.transitionDelay = `${index * 0.2}s`;
+            entry.target.classList.add("active");
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+const observerOptions = {
+    threshold: 0.5,
+}
+
+const observer = new IntersectionObserver(entryClassCb, observerOptions);
+
+document.querySelectorAll('.card-fade').forEach(ele => {
+    observer.observe(ele);
+});
